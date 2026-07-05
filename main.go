e0a51f9a68172da534a86b8a5ee29f41f57e497a
chanekarayush <chanekarayush@gmail.com>
2026-07-05 22:59:22 +0530

    feat: added scan dir function

--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
 )
 
+func scanForGitDir(folderPath string){
+	gitDirs := make([]string, 1)
+	err := filepath.WalkDir(folderPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err;
+		}
+		if (d.Name() == "node_modules" || d.Name() == "env" || d.Name() == "venv"){
+			return filepath.SkipDir
+		}
+		if (d.IsDir()){
+			println(path)
+		}
+		if (d.IsDir() && d.Name()==".git"){
+			gitDirs = append(gitDirs, path)
+			return filepath.SkipDir
+		}
+		return nil
+	})
+	if err != nil{
+		log.Fatal(err)
+	}
 
+	for i:=range gitDirs{
+		fmt.Printf("Scanned: %s\n", gitDirs[i])
+	}
+}
+
+/**
+* Scans all the subdirs of the given valid directory for `.git` folders
+* @param folderPath is the path of the parent folder for the subdirs with `.git` directories
+*/
 func scan (folderPath string)  {
-	fmt.Printf("Scanned %s:", folderPath)
+	fileinfo, err := os.Stat(folderPath)
+	if (err != nil || !fileinfo.IsDir()){
+		log.Fatalln("The given folder does not exist.\nPlease enter a valid Directory Path!")
+	}
+	scanForGitDir(folderPath)
 }
 
 func computeStats(stats int)  {
@@ -15,12 +53,18 @@ func computeStats(stats int)  {
 }
 
 func main()  {
-	folder := flag.String("add-dir", "", "Directory to be added for include in stats")
+	var folder string
+	var email string
+	flag.StringVar(&folder,"add-dir", "", "Directory to be added for include in stats")
+	flag.StringVar(&email,"email", "", "User Email to be searched for in the commits")
 	flag.Parse();
 
-	if (*folder != ""){
-		scan(*folder)
+	if (folder != ""){
+		scan(folder)
+		// if (email != ""){
+		// 	fmt.Printf("[Email]: %s\n",email)
+		// }
+		return
 	}
-	fmt.Println(*folder)
-
+	fmt.Println(folder)
 }

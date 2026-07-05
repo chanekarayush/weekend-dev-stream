8bf1c502f3a09491f0f82d2e94c2634bda3d1160
chanekarayush <chanekarayush@gmail.com>
2026-07-06 01:14:28 +0530

    feat: added the add/scan repository logic

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
+	"os/user"
 	"path/filepath"
+
 )
 
-func scanForGitDir(folderPath string){
+
+func scanForGitDir(folderPath string)([]string, error){
 	gitDirs := make([]string, 1)
 	err := filepath.WalkDir(folderPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
@@ -18,9 +22,6 @@ func scanForGitDir(folderPath string){
 		if (d.Name() == "node_modules" || d.Name() == "env" || d.Name() == "venv"){
 			return filepath.SkipDir
 		}
-		if (d.IsDir()){
-			println(path)
-		}
 		if (d.IsDir() && d.Name()==".git"){
 			gitDirs = append(gitDirs, path)
 			return filepath.SkipDir
@@ -28,11 +29,97 @@ func scanForGitDir(folderPath string){
 		return nil
 	})
 	if err != nil{
+		return nil, err
+	}
+	return gitDirs, nil
+}
+
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil{
 		log.Fatal(err)
 	}
+	dotFilePath := filepath.Join(usr.HomeDir, "/.local-git-heatmap")
+	return dotFilePath
+}
 
-	for i:=range gitDirs{
-		fmt.Printf("Scanned: %s\n", gitDirs[i])
+func parseLinesToSlice(dotFilePath string) ([]string, error) {
+	f, err := os.Open(dotFilePath)
+	if err != nil{
+		return nil, err
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	existingRepos := make([]string, 0)
+
+	for sc.Scan(){
+		existingRepos = append(existingRepos, sc.Text())
+	}
+	if err = sc.Err(); err != nil{
+		return nil, err
+	}
+
+	return existingRepos, nil
+}
+
+func mergeSlices(newRepos []string, existingRepos []string) ([]string, error)  {
+	repoMap := make(map[string]bool)
+	mergedRepos := make([]string, 0)
+
+	for i :=range existingRepos{
+		if _, ok := repoMap[existingRepos[i]]; ok == false{
+			repoMap[existingRepos[i]] = true
+			mergedRepos = append(mergedRepos, existingRepos[i])
+		}
+	}
+
+	for i :=range newRepos{
+		if _, ok := repoMap[newRepos[i]]; ok == false{
+			repoMap[newRepos[i]] = true
+			mergedRepos = append(mergedRepos, newRepos[i])
+		}
+	}
+	return mergedRepos, nil
+}
+
+func writeToFile(dotFilePath string, dedupedRepos []string) error{
+	file, err := os.Create(dotFilePath)
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+	for i:=range dedupedRepos{
+		_, err = file.WriteString(dedupedRepos[i]+"\n")
+		if err != nil{
+			return err
+		}
+	}
+	return nil
+}
+
+/** deduplicate files
+* write them to `dotFilePath`
+*/
+func addNewItemsToSlice(dotFilePath string, newRepos []string)  {
+	existingRepos, err := parseLinesToSlice(dotFilePath)
+	if err != nil {
+		log.Printf("Looks Like there was an error in reading %s\n", dotFilePath)
+		file, err := os.Create(dotFilePath)
+		if err != nil{
+			log.Fatal(err)
+		}else{
+			log.Printf("File was successfully created at %s\n", dotFilePath)
+		}
+		defer file.Close()
+	}
+	dedupedRepos, err := mergeSlices(newRepos, existingRepos)
+	if err != nil{
+		log.Fatal(err)
+	}
+	err = writeToFile(dotFilePath, dedupedRepos)
+	if err != nil{
+		log.Fatal(err)
 	}
 }
 
@@ -42,10 +129,13 @@ func scanForGitDir(folderPath string){
 */
 func scan (folderPath string)  {
 	fileinfo, err := os.Stat(folderPath)
-	if (err != nil || !fileinfo.IsDir()){
-		log.Fatalln("The given folder does not exist.\nPlease enter a valid Directory Path!")
+	if (err != nil || !fileinfo.IsDir()){log.Fatalln("The given folder does not exist.\nPlease enter a valid Directory Path!")}
+	folders, err := scanForGitDir(folderPath)
+	if err != nil{
+		log.Fatalln(err)
 	}
-	scanForGitDir(folderPath)
+	dotFilePath := getDotFilePath()
+	addNewItemsToSlice(dotFilePath, folders)
 }
 
 func computeStats(stats int)  {

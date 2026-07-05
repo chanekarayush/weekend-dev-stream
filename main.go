a76475187f61ff055d7442a0bca3178f135a57ab
chanekarayush <chanekarayush@gmail.com>
2026-07-05 20:35:56 +0530

    feat: Added flag
    Flag takes a string and if not null calls the scan function

--- main.go
+++ main.go
@@ -1,3 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"flag"
+)
 
+
+func scan (folderPath string)  {
+	fmt.Printf("Scanned %s:", folderPath)
+}
+
+func computeStats(stats int)  {
+	print(stats)	
+}
+
+func main()  {
+	folder := flag.String("add-dir", "", "Directory to be added for include in stats")
+	flag.Parse();
+
+	if (*folder != ""){
+		scan(*folder)
+	}
+	fmt.Println(*folder)
+
+}

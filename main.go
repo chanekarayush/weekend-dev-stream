6d6f941f55e39bdb48928e3775f6c763ba53a45e
chanekarayush <chanekarayush@gmail.com>
2026-07-11 18:58:07 +0530

    feat: updated const and linter changes

--- main.go
+++ main.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 )
 
-
-func main()  {
+func main() {
 	var folder string
 	var email string
-	flag.StringVar(&folder,"add-dir", "", "Directory to be added for include in stats")
-	flag.StringVar(&email,"email", "", "User Email to be searched for in the commits")
-	flag.Parse();
+	flag.StringVar(&folder, "add-dir", "", "Directory to be added for include in stats")
+	flag.StringVar(&email, "email", "", "User Email to be searched for in the commits")
+	flag.Parse()
 
-	if (folder != ""){
+	if folder != "" {
 		Scan(folder)
 	}
 	fmt.Println(folder)
-	Stats(email)
+	stats(email)
 }
--- statsUtils.go
+++ statsUtils.go
@@ -12,89 +12,108 @@ import (
 	"github.com/go-git/go-git/v6/plumbing/object"
 )
 
-const daysInLastSixMonths = 182
-const weeksInLastSixMonths = 26
-const outOfRange = 99999 
+const (
+	daysInLastSixMonths  = 183
+	weeksInLastSixMonths = 26
+	outOfRange           = 99999
+)
+
 type columns []int
 
-func Stats(email string)  {
+func stats(email string) {
 	commits := processRepos(email)
 	printCommits(commits)
 }
 
 /**
 * @param email of the user for which the commits are going to be scanned
-* @return map[int]int A map with number of commits per day for a 
+* @return map[int]int A map with number of commits per day for a
 * given length of time
-*/
+ */
 func processRepos(email string) map[int]int {
 	filePath := getDotFilePath()
 	repos, err := parseLinesToSlice(filePath)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	daysInMap := daysInLastSixMonths
 	commits := make(map[int]int)
-	for i:=1; i<=daysInMap; i++{
+	for i := 0; i <= daysInMap; i++ {
 		commits[i] = 0
 	}
 
-	for _, path := range repos{
+	for _, path := range repos {
 		commits = fillCommits(email, path, commits)
 	}
 	return commits
 }
 
-
 func fillCommits(email string, path string, commits map[int]int) map[int]int {
 	repo, err := git.PlainOpen(path)
-	if err != nil{
+	if err != nil {
+		if errors.Is(err, git.ErrRepositoryNotExists) {
+			fmt.Printf("\x1b[0;1;3m\x1b[38;2;204;2;2m Repository does not exist:\x1b[0m %s\n", path)
+			return commits
+		}
 		log.Panic(err)
 	}
 	ref, err := repo.Head()
-    if err != nil {
-        // Double-check both the sentinel error and the literal text string
-        if errors.Is(err, plumbing.ErrReferenceNotFound) || err.Error() == "reference not found" {
-            fmt.Printf("\x1b[0;1;3m\x1b[38;2;255;239;0m Skipping empty repository:\x1b[0m %s\n", path)
-            return commits // Make sure this returns 'commits', not 'nil'!
-        }
-        log.Panic(err)
-    }
+	if err != nil {
+		// Double-check both the sentinel error and the literal text string
+		if errors.Is(err, plumbing.ErrReferenceNotFound) || err.Error() == "reference not found" {
+			fmt.Printf("\x1b[0;1;3m\x1b[38;2;255;239;0m Skipping empty repository:\x1b[0m %s\n", path)
+			return commits
+		}
+		log.Panic(err)
+	}
 	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
-	if err != nil{
+	if err != nil {
 		log.Panic(err)
 	}
 
 	offset := calcOffset()
 	err = iterator.ForEach(func(c *object.Commit) error {
 		daysAgo := countDaysSinceDate(c.Author.When) + offset
-		if c.Author.Email != email{
+		if c.Author.Email != email {
 			return nil
 		}
-		if daysAgo != outOfRange{
+		if daysAgo != outOfRange {
 			commits[daysAgo]++
 		}
 		return nil
 	})
-	if err != nil{
+	if err != nil {
 		log.Panic(err)
 	}
-	
+
 	return commits
 }
 
+// func countDaysSinceDate(commitDate time.Time) int {
+//     now := getBeginningOfDay(time.Now())
+//     commitDayStart := getBeginningOfDay(commitDate)
+//
+//     // Normalize time zones to compare apples to apples
+//     days := int(now.Sub(commitDayStart).Hours() / 24)
+//
+//     if days < 0 || days > daysInLastSixMonths {
+//         return outOfRange
+//     }
+//     return days
+// }
 
 func countDaysSinceDate(commitDate time.Time) int {
-    now := getBeginningOfDay(time.Now())
-    commitDayStart := getBeginningOfDay(commitDate)
-    
-    // Normalize time zones to compare apples to apples
-    days := int(now.Sub(commitDayStart).Hours() / 24)
-    
-    if days < 0 || days >= daysInLastSixMonths {
-        return outOfRange
-    }
-    return days
+	now := getBeginningOfDay(time.Now())
+	commitDate = getBeginningOfDay(commitDate)
+	days := 0
+	for commitDate.Before(now) {
+		commitDate = commitDate.Add(24 * time.Hour)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+	return days
 }
 
 func getBeginningOfDay(currentTime time.Time) time.Time {
@@ -104,43 +123,61 @@ func getBeginningOfDay(currentTime time.Time) time.Time {
 }
 
 func calcOffset() int {
-	weekday := int(time.Now().Weekday())
-	return weekday
+	weekday := time.Now().Weekday()
+	offset := 0
+	switch weekday {
+	case time.Sunday:
+		offset = 7
+	case time.Monday:
+		offset = 6
+	case time.Tuesday:
+		offset = 5
+	case time.Wednesday:
+		offset = 4
+	case time.Thursday:
+		offset = 3
+	case time.Friday:
+		offset = 2
+	case time.Saturday:
+		offset = 1
+	}
+	return offset
 }
 
-func printCommits(commits map[int]int)  {
+func printCommits(commits map[int]int) {
 	keys := sortMapIntoSlice(commits)
 	cols := generateColumns(keys, commits)
 	printCells(cols)
-	
 }
 
 func sortMapIntoSlice(commits map[int]int) []int {
 	keys := make([]int, 0)
-	for key := range commits{
+	for key := range commits {
 		keys = append(keys, key)
 	}
 	sort.Ints(keys)
-	return keys	
+	return keys
 }
+
 func generateColumns(keys []int, commits map[int]int) map[int]columns {
 	cols := make(map[int]columns)
 	col := columns{}
-	for _, k := range keys{
-		week := int(k/7)
-		dayInWeek := k % 7 
-		if dayInWeek == 0{
+	for _, k := range keys {
+		week := int(k / 7)
+		dayInWeek := k % 7
+
+		if dayInWeek == 0 {
 			col = columns{}
 		}
+
 		col = append(col, commits[k])
 
-		if dayInWeek == 6{
+		if dayInWeek == 6 {
 			cols[week] = col
 		}
 	}
 	return cols
 }
-
 
 // printCells prints the cells of the graph
 func printCells(cols map[int]columns) {
@@ -151,7 +188,7 @@ func printCells(cols map[int]columns) {
 				printDayCol(j)
 			}
 			if col, ok := cols[i]; ok {
-				//special case today
+				// special case today
 				if i == 0 && j == calcOffset()-1 {
 					printCell(col[j], true)
 					continue
@@ -191,45 +228,47 @@ func printMonths() {
 // printCell given a cell value prints it with a different format
 // based on the value amount, and on the `today` flag.
 func printCell(val int, today bool) {
-    escape := "\033[0;37;30m"
-    switch {
-    case val > 0 && val < 5:
-        escape = "\033[1;30;47m"
-    case val >= 5 && val < 10:
-        escape = "\033[1;30;43m"
-    case val >= 10:
-        escape = "\033[1;30;42m"
-    }
+	escape := "\033[0;37;30m"
+	switch {
+	case val > 0 && val < 5:
+		escape = "\033[1;30;47m"
+	case val >= 5 && val < 10:
+		escape = "\033[1;30;43m"
+	case val >= 10:
+		escape = "\033[1;30;42m"
+	}
 
-    if today {
-        escape = "\033[1;37;45m"
-    }
+	if today {
+		escape = "\033[1;37;45m"
+	}
 
-    if val == 0 {
-        fmt.Print(escape + "  - " + "\033[0m")
-        return
-    }
+	if val == 0 {
+		fmt.Print(escape + "  - " + "\033[0m")
+		return
+	}
 
-    str := "  %d "
-    switch {
-    case val >= 10:
-        str = " %d "
-    case val >= 100:
-        str = "%d "
-    }
+	str := "  %d "
+	switch {
+	case val >= 10:
+		str = " %d "
+	case val >= 100:
+		str = "%d "
+	}
 
-    fmt.Printf(escape+str+"\033[0m", val)
+	fmt.Printf(escape+str+"\033[0m", val)
 }
 
 func printDayCol(day int) {
 	out := "     "
 	switch day {
-	case 1:
-		out = " Mon "
-	case 3:
-		out = " Wed "
-	case 5:
-		out = " Fri "
+	case 0:
+		out = " Sat "
+	case 2:
+		out = " Tue "
+	case 4:
+		out = " Thu "
+	case 6:
+		out = " Sun "
 	}
 
 	fmt.Print(out)

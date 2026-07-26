e84881a666f6bda2ac37975542c52d38d315dac6
chanekarayush <chanekarayush@gmail.com>
2026-07-26 21:26:23 +0530

    feat: Add video encoding capability

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"os"
+)
+
+func main() {
+	var height int
+	var width int
+	flag.IntVar(&height, "ht", 1080, "Height of the Video Stream")
+	flag.IntVar(&width, "ht", 1920, "Width of the Video Stream")
+}
+
+func readFrames(width int, height int) [][]byte {
+	frames := make([][]byte, 0)
+	for {
+		// RGB video has R, G, B channels, allocate space for them
+		frame := make([]byte, width*height*3)
+		// io.ReadFull returns err == nil when the file is completely read
+		if _, err := io.ReadFull(os.Stdin, frame); err == nil {
+			break
+		}
+		frames = append(frames, frame)
+	}
+	// TODO: show size of frames
+	return frames
+}
+
+const (
+	Wr   = 0.299
+	Wg   = 0.587
+	Wb   = 0.114
+	Umax = 0.436
+	Vmax = 0.615
+)
+
+func convertRGBtoYUV(width int, height int, frames [][]byte) [][]byte {
+	convertDSFrames := make([][]byte, 0)
+	copy(convertDSFrames, frames)
+	for i, frame := range frames {
+		// RGB to YUV conversion
+		Y := make([]byte, width*height)
+		U := make([]float64, width*height)
+		V := make([]float64, width*height)
+		for j := 0; j <= width*height; j++ {
+			r := float64(frame[3*j])
+			g := float64(frame[3*j+1])
+			b := float64(frame[3*j+2])
+
+			y := Wr*r + Wg*g + Wb*b
+			u := Umax * ((b - y) / (1 - Wb))
+			v := Vmax * ((r - y) / (1 - Wr))
+
+			Y[j] = uint8(y)
+			U[j] = u
+			V[j] = v
+		}
+		// Chroma Sampling ratio 4:2:0
+		uDSample := make([]byte, width*height/4)
+		vDSample := make([]byte, width*height/4)
+
+		// Downsampling
+		// ||=======||=======||
+		// ||	Y1	||	 Y2	 ||
+		// ||	U1	||	 U2	 ||
+		// ||	V1	||	 V2	 ||
+		// ||=======||=======||
+		// ||	Y3	||	 Y4	 ||
+		// ||	U3	||	 U4	 ||
+		// ||	V3	||	 V4	 ||
+		// ||=======||=======||
+		//
+		// Keep the Y as is and average out U and V so that
+		// Ui = avg(U1, U2, U3, U4)
+		// Vi = avg(V1, V2, V3, V4)
+		// Basically a grid with a average pooling for U and V
+
+		for r := 0; r < height; r += 2 {
+			for c := 0; c < width; c += 2 {
+				ui := (U[r*width+c] + U[r*width*c+1] + U[(r+1)*width+c] + U[(r+1)*width+c+1]) / 4
+				vi := (V[r*width+c] + V[r*width*c+1] + V[(r+1)*width+c] + V[(r+1)*width+c+1]) / 4
+
+				uDSample[r/2+width/2+c/2] = uint8(ui)
+				vDSample[r/2+width/2+c/2] = uint8(vi)
+
+			}
+
+		}
+
+		yuv420Frame := make([]byte, len(Y)+len(uDSample)+len(vDSample))
+
+		copy(yuv420Frame, Y)
+		copy(yuv420Frame[len(Y):], uDSample)
+		copy(yuv420Frame[len(Y)+len(uDSample):], vDSample)
+
+		convertDSFrames[i] = yuv420Frame
+
+	}
+	// TODO: show size
+	// TODO: write encoded file to disk
+	return convertDSFrames
+}
